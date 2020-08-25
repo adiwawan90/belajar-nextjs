d9e94a3b1c1761b14d51ec137cc9dd7b4a02fcd5
adiwawan90 <adi.wawan90@gmail.com>
2020-08-25 11:31:37 +0700

    add product card

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,32 @@
 import Layout from '@common/components/layout'
 import Link from 'next/link'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import ProductCard from '@common/components/ProductCard'
 
 const HomePage = ({ products }) => {
   const { list } = products;
   return (
     <Layout>
-
-      <div className="container">
-
-        <Link href="/about" >
-          <a>Menuju About Page</a>
-        </Link>
-        {list.map((item) => {
-          return (
-            <div key={item.id}>
-              <ul>
-                <li>{ item.name }</li>
-              </ul>
-            </div>
-          )
-        })}
-
-
-      </div>
+      <Container maxWidth="sm">
+        <Grid container spacing={2}>
+          {list.map((item) => {
+            return (
+              <Grid key={item.id} item xs={6} sm={6}>
+                <ProductCard 
+                  productID={item.id}
+                  img={item.img}
+                  title={item.name}
+                  promoLabel={item.promo}
+                  price={item.price}
+                  rating={item.rating}
+                  sold={item.sold}
+                />
+              </Grid>
+            )
+          })}
+        </Grid>
+      </Container>
     </Layout>
   )
 }
